34d153b49b63873cfc513e960a202de0a06045d6
Jason Franklin <jfrank325@gmail.com>
2020-02-03 19:17:11 +0100

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Movie = require('../models/Movie');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find({})
+    .then(movies => {
+      console.log(movies);
+      res.render('movies', { data: movies });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get('/movie/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(movie => {
+      res.render('movie', { data: movie });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
